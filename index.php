f903089e8bb19c3cbbf08e18905abdcc546a4a9a
Sylvie-B <unsolo@hotmail.fr>
2022-01-12 16:39:34 +0100

    je dors enfin

--- index.php
+++ index.php
@@ -34,3 +34,22 @@ echo "Je dors enfin...<br><br>";
 
 // TODO Votre code ici !
 
+$nbrVache = 1;
+echo "1.Je vais compter les vaches pour dormir : <br>";
+while ($nbrVache <= 25){
+    echo $nbrVache . " vache(s) <br>";
+    $nbrVache++;
+}
+echo "Je dors enfin...<br><br>";
+
+$nbrVache2 = 1;
+$dormir = false;
+echo "2.Je vais compter les moutons pour dormir : <br>";
+while ($dormir === false){
+    echo $nbrVache2 . " vache(s) <br>";
+    $nbrVache2++;
+    if($nbrVache2 > 25){
+        $dormir = true;
+    }
+}
+echo "Je dors enfin...<br><br>";
